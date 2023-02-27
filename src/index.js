f976e1483a4a711bedda6342a7b997d9ad5f12ce
islagit <islamsandybayev24@gmail.com>
2023-02-27 14:32:06 +0600

    code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const openingBrackets = ['(', '[', '{', '1', '2', '3', '5', '6', '7', '8', '9'];
+  const closingBrackets = [')', ']', '}', '1', '2', '3', '5', '6', '7', '8', '9'];
+
+  for (let i = 0; i < str.length; i++) {
+    const bracket = str[i];
+    if (openingBrackets.includes(bracket)) {
+      stack.push(bracket);
+    } else if (closingBrackets.includes(bracket)) {
+      const lastOpening = stack.pop();
+      if (openingBrackets.indexOf(lastOpening) !== closingBrackets.indexOf(bracket)) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
